8bf0f16a92135f6216258b2b79a3049287be3f70
yiludege <xzd123@qq.com>
2024-10-07 22:38:48 +0800

    refactor: separate docs

---
+++ index.ts
@@ -0,0 +1,4 @@
+export type * from './src/observer'
+export type * from './src/rootObserver'
+export * from './src/stream'
+export * from './src/operator'
--- packages/fluth/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './src/stream'
-export * from './src/operator'
